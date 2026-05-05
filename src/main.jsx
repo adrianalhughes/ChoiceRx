846fce66e069d1988b5f0642caf93ef7f4b90723
Adriana Hughes <kitty@Adrianas-Air.lan>
2026-05-05 10:20:59 -0400

    Remove password gate and add monthly formulary workflow

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import AccessGate from './AccessGate'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <AccessGate>
-    <App />
-  </AccessGate>
+  <App />
 )
